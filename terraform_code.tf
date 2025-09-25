eebbd50a385f84c14999f91947949180adb1cff7
Ahmed Nas <105170474+Ahmednas211@users.noreply.github.com>
2025-09-25 18:20:00 -0500

    Update terraform_code.tf

--- terraform_code.tf
+++ terraform_code.tf
@@ -1,1 +1,112 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = " >=5.40.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+variable "uhg-cidrs" {
+  type = list(string)
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-2a"
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.uhg-cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Data source for Optum Golden ECS AMI
+data "aws_ami" "optum_golden_ecs" {
+  most_recent = true
+  owners      = ["187829367669"]
+
+  filter {
+    name   = "name"
+    values = ["optum/AmazonLinux_2023*"]
+  }
+}
+
+# EC2 Instance using Optum Golden ECS AMI
+resource "aws_instance" "optum_golden_ecs_instance" {
+  ami                    = data.aws_ami.optum_golden_ecs.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  subnet_id              = aws_subnet.main.id
+  iam_instance_profile   = "ec2-ssm-access-role"
+  tags = {
+    Name = "AAA-Demo2"
+  }
+}
 
